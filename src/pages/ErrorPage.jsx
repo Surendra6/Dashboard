32c6c7b22796a3e975a28efc67db3cf457e8bbde
Surendra Gullipalli <surendra.gullipalli6@gmail.com>
2024-10-13 10:54:03 +0100

    Updated home path in error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -11,7 +11,7 @@ const ErrorPage = () => {
         <p className="mt-4 text-gray-500">{`We can't find that page.`}</p>
 
         <a
-          href="/blog-posts"
+          href="/Dashboard"
           className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
         >
           Go Back Home
